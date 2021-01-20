3e9e5bcf87cbfcf46e06aacfefe4cc7b4720ddd0
xyzou <player@163.com>
2021-01-21 00:32:12 +0800

    update

--- slider_captcha.py
+++ slider_captcha.py
@@ -13,60 +13,28 @@ from PyQt5.QtGui import *
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
-class MySlider(QSlider):
-    def __init__(self, orientation, parent=None):
-        super(MySlider, self).__init__(orientation, parent)
-        self.setFixedHeight(40)
-        self.setMaximum(100)
-
-        self.label = QLabel("向右滑动滑块验证", self)
-        self.label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        self.setLayout(layout)
-
-    def mousePressEvent(self, event):
-        self.label.hide()
-
-    def mouseMoveEvent(self, event):
-        value = (event.pos().x() - self.x()) / (self.width() - self.x()) * 100
-        self.setValue(value)
-
-    def mouseReleaseEvent(self, event):
-        pass
-        # self.setValue(0)
-        # self.label.show()
 
 class SliderCaptchaDialog(QDialog):
     def __init__(self):
         try:
             super(SliderCaptchaDialog, self).__init__()
-            # self.resize(300, 220)
+            self.resize(380, 220)
             self.setWindowFlags(Qt.WindowCloseButtonHint)
             self.setWindowTitle("滑动验证码")
 
-            # 随即图片
-            width = random.randrange(300, 400, 2)
-            height = width // 2
-            url = f"http://placekitten.com/{width}/{height}"
-            logging.debug(f"captcha image url: {url}")
+            self.label_puzzle = QLabel()
+            self.thread_load_image = LoadImageThread()  # 创建线程
+            self.thread_load_image.start()
+            self.thread_load_image.trigger.connect(self.load_image)
 
-            self.label_puzzle = QLabel("123")
-            # try:
-            #     image_data = requests.get(url).content
-            # except:
-            #     image_data = None
-            # pixmap = QPixmap()
-            # pixmap.loadFromData(image_data)
-            # self.label_puzzle.setPixmap(pixmap)
-            # self.label_puzzle.setScaledContents(True)
-
-            self.slider = MySlider(Qt.Horizontal)
+            self.slider = QSlider(Qt.Horizontal)
+            self.slider.setFixedHeight(40)
 
             self.vbox = QVBoxLayout(self)
             self.vbox.addWidget(self.label_puzzle)
             self.vbox.addWidget(self.slider)
+            self.vbox.setStretch(0, 1)
+            self.vbox.setStretch(1, 0)
 
             self.setStyleSheet("""
 QDialog {
@@ -82,20 +50,59 @@ QSlider::groove {
 }
 
 QSlider::handle:horizontal {
-	background:qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #b4b4b4, stop:1 #8f8f8f);
-	border:2px solid #5C5C5C;
-	border-radius:5px;
-	width:18px;
-	
-	/* 滑块上下边与滑道重合 */
-	margin:-2px 0;
+    image: url(./icons/arrow.png);
+    border:1px solid #999999;
+    background: #ffffff;
+    width:30px;
+    margin: -1px ;
+}
+
+QSlider::handle:hover {
+    image: url(./icons/arrow-hover.png);
+    border:1px solid #999999;
+    background: #1991FA;
+}
+
+QSlider::add-page:horizontal {
+}
+
+QSlider::sub-page:horizontal {
+    border: 1px solid  #1991FA;
+    background: #D1E9FE;
 }
       """)
 
         except:
             traceback.print_exc()
 
+    def load_image(self, content):
+        try:
+            pixmap = QPixmap()
+            pixmap.loadFromData(content)
+            pixmap = pixmap.scaled(self.label_puzzle.size())
+            self.label_puzzle.setPixmap(pixmap)
+            self.label_puzzle.setScaledContents(True)
+        except:
+            traceback.print_exc()
 
+class LoadImageThread(QThread):
+
+    trigger =pyqtSignal(bytes)
+
+    def __init__(self):
+        super(LoadImageThread, self).__init__()
+
+    def run(self):
+        width = random.randrange(300, 400, 2)
+        height = width // 2
+        url = f"http://placekitten.com/{width}/{height}"
+        logging.debug(f"captcha image url: {url}")
+
+        try:
+            image_data = requests.get(url).content
+        except:
+            image_data = None
+        self.trigger.emit(image_data)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
